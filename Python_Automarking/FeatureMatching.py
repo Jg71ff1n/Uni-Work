a27d56c307e40452e2ae512a4daed5f892d4c73d
jg71ff1n <jcgriffin999@gmail.com>
2018-03-24 13:29:38 +0000

    Added Homography and fixed cv/cv2 referencing

--- Python_Automarking/FeatureMatching.py
+++ Python_Automarking/FeatureMatching.py
@@ -2,13 +2,15 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plot
 
+MinimumMatchCount = 10
+
 def keyPointFinderORB(image):
 	"Finds key points based on an input image using ORB feature detection"
 	image = cv2.imread(image,0)
 	orb = cv2.ORB_create()
 	keyPoints = orb.detect(image,None)
 	keyPoints, descriptors = orb.compute(image,keyPoints)
-	output = cv.drawKeypoints(image, keyPoints, None, color=(0,255,0), flags=0)
+	output = cv2.drawKeypoints(image, keyPoints, None, color=(0,255,0), flags=0)
 	plot.imshow(output), plot.show()
 	return keyPoints, descriptors
 
@@ -16,7 +18,7 @@ def keyPointFinderSIFT(image):
 	image = cv2.imread(image,0)
 	sift = cv2.SIFT()
 	keyPoints, descriptors = sift.detectAndCompute(image,None)
-	output = cv.drawKeypoints(image, keyPoints, None, color=(0,255,0), flags=0)
+	output = cv2.drawKeypoints(image, keyPoints, None, color=(0,255,0), flags=0)
 	plot.imshow(output), plot.show()
 	return keyPoints, descriptors
 
@@ -32,7 +34,7 @@ def bruteForceMatcher(inputImage,comparisonImage):
 	matches = bfMatcher.match(inputDescriptors,comparisonDescriptors)
 	matches = sorted(matches, key = lambda x:x.distance)
 	# Draw first 10 matches.
-	outputImage = cv.drawMatches(inputImage,inputKeyPoints,comparisonImage,comparisonKeyPoints,matches[:10], flags=2)
+	outputImage = cv2.drawMatches(inputImage,inputKeyPoints,comparisonImage,comparisonKeyPoints,matches[:10], flags=2)
 	plot.imshow(outputImage),plot.show()
 	return matches.distance
 
@@ -48,15 +50,29 @@ def FLANNMatcher(inputImage,comparisonImage):
 	flann = cv2.FlannBasedMatcher(index_params,search_params)
 
 	matches = flann.knnMatch(inputDescriptors,comparisonDescriptors,k=2)
-	matchesMask = [[0,0] for i in xrange(len(matches))]
-	for i,(m,n) in enumerate(matches):
-		if m.distance < 0.7*n.distance:
-			matchesMask[i]=[1,0]
+	good = []
+	for m,n in matches:
+    	if m.distance < 0.7*n.distance:
+        	good.append(m)
+        	
+    if len(good)>MinimumMatchCount:
+    	if len(good)>MIN_MATCH_COUNT:
+		    src_pts = np.float32([ inputKeyPoints[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+		    dst_pts = np.float32([ comparisonKeyPoints[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+		    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+		    matchesMask = mask.ravel().tolist()
+		    h,w,d = inputImage.shape
+		    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+		    dst = cv2.perspectiveTransform(pts,M)
+		    outputImage = cv2.polylines(outputImage,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+		else:
+		    print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+		    matchesMask = None
+		draw_params = dict(
+					matchColor = (0,255,0), # draw matches in green color
+            		singlePointColor = None,
+            		matchesMask = matchesMask, # draw only inliers
+            		flags = 2)
 
-	draw_params = dict(
-					matchColor = (0,255,0),
-               		singlePointColor = (255,0,0),
-                   	matchesMask = matchesMask,
-                   	flags = 0)
-	outputImage = cv2.drawMatchesKnn(inputImage,inputKeyPoints,outputImage,comparisonKeyPoints,matches,None,**draw_params)
-	plot.imshow(outputImage),plot.show()
+	outputImage = cv2.drawMatchesKnn(inputImage,inputKeyPoints,outputImage,comparisonKeyPoints,good,None,**draw_params)
+	plot.imshow(outputImage, 'gray'),plot.show()
